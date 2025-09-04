92e65147bc08f5130643d542bdcba59450d4d559
felipesfl <felipeferreira.desenvolvedor@gmail.com>
2025-09-03 21:21:11 -0300

    desafio 05 - commit

---
+++ desafio_tec5.js
@@ -0,0 +1,12 @@
+function calcularArea(base, altura){
+    const area = base * altura
+    return area
+}
+
+function calcularPerimetro(lado1, lado2, lado3, lado4){
+    const perimetro = lado1 + lado2 + lado3 + lado4
+    return perimetro
+}
+
+console.log(calcularArea(7.5, 7.5))
+console.log(calcularPerimetro(8, 5, 6, 9))
