1588283bf7fdbd537d2dbb0feecbc8b6990c9f06
felipesfl <felipeferreira.desenvolvedor@gmail.com>
2025-09-03 21:15:31 -0300

    ajustes no desafio 03 e desafio 04 - commit

--- desafio_tec3.js
+++ desafio_tec3.js
@@ -1,5 +1,5 @@
-function mensagem(nome, idedade, cidade){
-    return print(`Olá ${nome}, a sua idade é ${idade}, e a sua cidade é ${cidade}`)
+function mensagem(nome, idade, cidade){
+    return `Olá ${nome}, a sua idade é ${idade}, e a sua cidade é ${cidade}`
 }
 
 console.log(mensagem(Felipe, 20, Altaneira))
---
+++ desafio_tec4.js
@@ -0,0 +1,9 @@
+function verDivisibilidade(x, y){
+    if(x % y == 0){
+        console.log("O x e y são divisiveis")
+    } else{
+        console.log("O x e y não são divisiveis")
+    }
+}
+
+console.log(verDivisibilidade(5, 8))
